3fef3081024c2bb1168c0cd2c600222fabb104b5
canonale <canonale@gmail.com>
2018-01-21 10:21:35 +0100

    Added minus method

--- calculator.py
+++ calculator.py
@@ -1,4 +1,12 @@
 class Calculator(object):
 
     def add(self, x, y):
-        return x+y
+        return x + y
+
+    def Minus(self, X, Y):
+        number_types = (int, long, float, complex)
+
+        if isinstance(x, number_types) and isinstance(y, number_types):
+            return X - Y
+        else:
+            raise ValueError
